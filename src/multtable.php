e12a25513077846cecdd60efbd8352660a89e530
kosloffd <kosloffd@onid.oregonstate.edu>
2015-05-02 19:56:38 -0700

    Three hours: implementing data validation for multtable and creating loops for table. git status

---
+++ src/multtable.php
@@ -0,0 +1,55 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+header('Content-Type: text/html');
+?>
+
+<!DOCTYPE html>
+	<title>Multiplication Table</title>
+
+	<?php
+		//All the input validation performed first
+		foreach($_GET as $key => $value)
+		{
+			if($value == "")
+			{
+				echo "Missing parameter: $key<br>";
+			}
+			if(!(is_numeric($value) && $value > 0))
+			{
+				echo "<br>$key must be an integer larger than zero.<br>";
+			}
+		}
+
+		$minMultiplicand = $_GET["min-multiplicand"];
+		$maxMultiplicand = $_GET["max-multiplicand"];
+		$minMultiplier = $_GET["min-multiplier"];
+		$maxMultiplier = $_GET["max-multiplier"];
+
+		//Check whether the variables are comparatively less or more
+		if(!($minMultiplicand < $maxMultiplicand))
+		{
+			echo "The min-multiplicand value is larger than the max-multiplicand value.";
+		}
+		else if(!($minMultiplier < $maxMultiplier))
+		{
+			echo "The min-multiplier value is larger than the max-multiplier value.";
+		}
+		else
+		{
+			$height = $maxMultiplicand - $minMultiplicand + 2;
+			$width = $maxMultiplier - $minMultiplier + 2;
+			echo <table>;
+			for($col = 0; $col<$height; $col ++)
+			{
+				echo "<tr>";
+				//if col = 0, echo "<td>";
+				//else echo "<td>$col;"
+				for($row = 0; $row<$width; $row++)
+				{
+					//if col = 0, echo "<td>$row"
+					//else, echo "<td>$row*$col;" 
+				}
+			}
+		}
+?>
