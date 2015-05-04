dfeb99dddbfa6a8f1ee30749319300fdf7613c77
kosloffd <kosloffd@onid.oregonstate.edu>
2015-05-03 22:02:31 -0700

    About 4 hours spent completing assignment.  Mostly stuck on control structures for the redirect to login and ending the session correctly.

--- src/content1.php
+++ src/content1.php
@@ -3,10 +3,64 @@
 	ini_set('display_errors', true);
 	header('Content-Type: text/html');
 
-	echo "<!DOCTYPE html>";
-	echo "<title>JSON String</title>";
+	//Check to see if they have entered a username
+	if(isset($_POST["username"]) && ($_POST["username"] == ""))
+	{
+		echo "You must enter a user name.<br>Click <a href=\"login.php\">here</a> to return to the login screen.<br>";
+	}
 
-	$name = $_POST["username"];
-	echo "Hi $name, you have visited this page n times before.<br>";
-	echo "Click <a href=\"login.php\">here</a> to logout.";
+	
+	
+	else
+	{
+		session_start();
+
+		//If the name hasn't been entered before, store in session name
+		if(!isset($_SESSION["name"]) && isset($_POST["username"]))
+		{	
+			$_SESSION["name"] = $_POST["username"];
+			$_SESSION["valid"] = "true";
+		}
+
+		if(isset($_SESSION["valid"]) && $_SESSION["valid"] == "true")
+		{
+		
+			//If a logout=true was sent by GET, close it down
+			if(isset($_GET["logout"]) && $_GET["logout"] == "true")
+			{
+				$_SESSION = array();
+				session_destroy();
+				header("Location: login.php", $replace = true);
+				exit();
+			}
+
+			//Set visit log up if not set up yet
+			if(!isset($_SESSION["visits"]))
+			{
+				$_SESSION["visits"] = 0;
+			}
+
+			echo "Hi $_SESSION[name], you have visited this page $_SESSION[visits] times before.<br>";
+			echo "Click <a href=\"content2.php?referrer=valid\">here</a> to go to page 2.<br><br>";
+			echo "Click <a href=\"content1.php?logout=true\">here</a>to logout.";
+			$_SESSION["visits"] ++;
+		}
+		else
+		{
+			header("Location: login.php", $replace = true);
+		}
+	}	
+
+
+
+
+
+		
+
+
+	//need to make sure name stays in $_SESSION when I revisit this page (not redirect to login)
+	//need to set up content2 page on $_SESSION["visits"] >=1 and redirect if no previous session
 ?>
+	<!-- <form action="content1.php", method="GET">
+	<button name="logout", value="true", type="submit">Logout</button>
+</form> -->
--- src/content2.php
+++ src/content2.php
@@ -2,5 +2,25 @@
 error_reporting(E_ALL);
 ini_set('display_errors', true);
 header('Content-Type: text/html');
+
+session_start();
+if(isset($_GET["referrer"]) && $_GET["referrer"] == "valid")
+{	
+	if(!isset($_SESSION["valid"]))
+	{
+		$_SESSION["valid"] = "true";
+	}
+}
+
+if(isset($_SESSION["valid"]) && $_SESSION["valid"] == "true")
+{
+	echo "<h2>Welcome to Page Two!<br><h3>We've been expecting you.<br><br>";
+	echo "Click <a href=\"content1.php\">here</a> to go back to page one.";
+}
+
+else
+{
+	header("Location: login.php", $replace = true);
+}
 ?>
 
